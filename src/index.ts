11bd4385da759f8fd96e515b5be38c056c2a4420
gnostichumor <gnostichumor@gmail.com>
2022-05-20 09:28:34 -0400

    updated index.ts and create new scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 import { schema } from "./schema";
 import { context } from "./context";
 
 export const server = new ApolloServer({
     schema,
-    context
+    context,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen({ port }).then(({ url }) => {
     console.log(`Server ready at ${url}`);
